e249267fa09ede09eba4c5b0e05530f937c377be
Antoine Lehurt <keuwah@gmail.com>
2013-02-03 21:24:08 +0100

    Create `importsrc` task

---
+++ tasks/importsrc.js
@@ -0,0 +1,17 @@
+/*
+ * grunt-importsrc
+ * https://github.com/kewah/grunt-importsrc
+ *
+ * Copyright (c) 2013 Antoine Lehurt
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('importsrc', 'Import scripts and stylesheets sources from HTML files', function() {
+
+  });
+
+};
