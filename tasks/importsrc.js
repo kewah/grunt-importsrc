dba6156154eb7c581bf3c70c36bc542ea6fe7568
Antoine Lehurt <keuwah@gmail.com>
2013-02-04 16:34:10 +0100

    Add `importsrc` update task

--- tasks/importsrc.js
+++ tasks/importsrc.js
@@ -28,6 +28,31 @@ module.exports = function(grunt) {
     return path.join(htmlRootPath, filepath);
   }
 
+  /**
+   * Convert a string ('path.to.smth') to an object (path: {to: {smth: {}}})
+   * @param  {String} str
+   * @return {Object}
+   */
+
+  function stringToGruntConfigObject(str) {
+    var cfg = grunt.config.data;
+    var props = str.split('.');
+    var obj, lastProp;
+
+    props.forEach(function(prop, i) {
+      if (i === props.length - 1) {
+        lastProp = prop;
+        return;
+      }
+      obj = (!obj) ? cfg[prop] : obj[prop];
+    });
+
+    return {
+      obj: obj,
+      lastProp: lastProp
+    };
+  }
+
   // `importsrc` task
   grunt.registerMultiTask('importsrc', 'Import file paths of scripts and stylesheets from HTML files', function() {
     this.files.forEach(function(file) {
@@ -72,8 +97,7 @@ module.exports = function(grunt) {
       }
 
       if (updateParam) {
-        // TODO: update task
-        return;
+        outputFilepath = updateGruntTask(section, updateParam);
       }
 
       // replace section with the output file path.
@@ -122,6 +146,93 @@ module.exports = function(grunt) {
     grunt.log.writeln('\n');
 
     return concatDest;
+  }
+
+  /**
+   * Update an existing Grunt task (like uglify, mincss, etc.)
+   * @param  {String} section
+   * @param  {String} taskToUpdate
+   * @return {String}              The output file path.
+   */
+
+  function updateGruntTask(section, taskToUpdate) {
+    var destFilepath = util.extractReplaceParam(section);
+    var destFileExtension = util.getFileExtension(destFilepath);
+
+    // Grunt has different syntax format for config of tasks (see https://github.com/gruntjs/grunt/wiki/grunt)
+    // - compact & list: 'dist/built.js': ['src/file1.js', 'src/file2.js']
+    //    ex: uglify.dist.files['dist/built.js'] (<= brackets) return ['src/file1.js', 'src/file2.js']
+    // - full: env: {src: ['src/file1.css', 'src/file2.css'], dest: ... }
+    //    ex: mincss.compress.src (<= no brackets) return ['src/file1.css', 'src/file2.css']
+    //
+    // To detect those different formats, I just check if there are brackets in the `taskToUpdate` value.
+    if (util.containsBrackets(taskToUpdate)) {
+      updateGruntTaskWithCompactFormat(section, taskToUpdate, destFileExtension);
+    } else {
+      updateGruntTaskWithFullFormat(section, taskToUpdate, destFileExtension);
+    }
+
+    return destFilepath;
+  }
+
+  function updateGruntTaskWithCompactFormat(section, taskToUpdate, destFileExtension) {
+    var insideBrackets = util.extractValueInsideBrackets(taskToUpdate);
+    var cfg = stringToGruntConfigObject(util.removeBrackets(taskToUpdate));
+    var taskData = cfg.obj[cfg.lastProp];
+    var taskDestFile;
+
+    // get the last prop of the object to be able to update it after.
+    insideBrackets.forEach(function(prop, i) {
+      if (i === insideBrackets.length - 1) {
+        taskDestFile = prop;
+        return;
+      }
+
+      taskData = taskData[prop];
+    });
+
+    if (!taskData[taskDestFile]) {
+      taskData[taskDestFile] = [];
+    }
+
+    // extract file paths from the html to add it to the task that we are updating.
+    var sources = util.extractFilePaths(section, destFileExtension).map(addRootPathTo);
+    var updatedTask = _.union(taskData[taskDestFile], sources);
+
+    updateGruntTaskMessage(taskToUpdate, taskData[taskDestFile], updatedTask);
+  }
+
+  function updateGruntTaskWithFullFormat(section, taskToUpdate, destFileExtension) {
+    var cfg = stringToGruntConfigObject(taskToUpdate);
+    var taskData = cfg.obj[cfg.lastProp];
+
+    if (!taskData) {
+      taskData = cfg.obj[cfg.lastProp] = [];
+    }
+
+    // extract file paths from the html to add it to the task that we are updating.
+    var sources = util.extractFilePaths(section, destFileExtension).map(addRootPathTo);
+    var updatedTask = _.union(taskData, sources);
+
+    updateGruntTaskMessage(taskToUpdate, taskData, updatedTask);
+  }
+
+  /**
+   * Display a message in the terminal.
+   */
+
+  function updateGruntTaskMessage(taskName, beforeUpdate, afterUpdate) {
+    grunt.log.ok('Task ' + taskName + ' has been updated');
+    grunt.log.subhead('  from:');
+    grunt.log.writeln('    - ' + grunt.log.wordlist((_.isArray(beforeUpdate)) ? beforeUpdate : [beforeUpdate], {
+      separator: '\n    - '
+    }));
+    grunt.log.subhead('  to:');
+    grunt.log.writeln('    - ' + grunt.log.wordlist(afterUpdate, {
+      separator: '\n    - '
+    }));
+
+    grunt.log.writeln('\n');
   }
 
 };
