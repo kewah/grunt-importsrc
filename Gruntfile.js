9dd8efc12f03179af8affcad0b71912bc7bdb768
Antoine Lehurt <keuwah@gmail.com>
2013-02-04 18:17:51 +0100

    Add expected output files for importsrc unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,9 +20,18 @@ module.exports = function(grunt) {
       tests: ['test/*_test.js']
     },
     uglify: {
-      dist: {
+      test: {
         files: {
           'temp/scripts/uglify_output.js': []
+        }
+      },
+      expected: {
+        files: {
+          'test/expected/scripts/uglify_output.js': [
+            'test/fixtures/scripts/vendors/plugin.js',
+            'test/fixtures/scripts/file-1.js',
+            'test/fixtures/scripts/file-2.js'
+          ]
         }
       },
       options: {
@@ -30,9 +39,17 @@ module.exports = function(grunt) {
       }
     },
     mincss: {
-      compress: {
+      test: {
         src: ['test/fixtures/styles/input.css'],
         dest: 'temp/styles/mincss_output.css'
+      },
+      expected: {
+        src: [
+          'test/fixtures/styles/input.css',
+          'test/fixtures/styles/reset.css',
+          'test/fixtures/styles/main.css'
+        ],
+        dest: 'test/expected/styles/mincss_output.css'
       }
     },
 
@@ -54,7 +71,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-mincss');
 
-  grunt.registerTask('test', ['clean', 'importsrc', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'importsrc', 'uglify:test', 'mincss:test','nodeunit']);
+
+  // build expected files to check if the `updatesrc:update` is working.
+  grunt.registerTask('expected', ['uglify:expected', 'mincss:expected']);
 
   grunt.registerTask('default', ['jshint', 'test']);
 };
---
+++ test/expected/scripts/importsrcs-concat_output.js
@@ -0,0 +1,10 @@
+// This is a plugin
+(function(window) {
+  console.log('Plugin');
+}(window));
+(function(window) {
+  console.log('File 1');
+}(window));
+(function(window) {
+  console.log('File 2');
+}(window));
